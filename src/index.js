748b1140f069de0f9ae78ccde4eff0ff2d7b19a5
unknown <vaibhavpimpalkar28@gmail.com>
2022-10-08 15:25:51 +0530

    final

--- src/index.js
+++ src/index.js
@@ -20,3 +20,7 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+
+
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,9 +2,7 @@ const express = require('express');
 const { shortUrl, getUrl } = require('../Controllers/urlController');
 const router = express.Router();
 
-router.get("/test-me", function (req, res) {
-    return res.send({ message: "my first ever api" })
-})
+
 
 
 router.post("/url/shorten", shortUrl)
@@ -12,9 +10,6 @@ router.post("/url/shorten", shortUrl)
 router.get("/:urlCode", getUrl)
 
 
-router.all('/*', function (req, res) {
-    res.status(404).send({ status: false, message: 'path not found' })
-})
 
 
 module.exports = router
